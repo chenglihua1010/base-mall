f2adc5863c94038e6da89674f407123ee26b9850
chenglihua1010 <1414592108@qq.com>
2020-06-21 22:51:06 +0800

    框架搭建

--- src/main/java/com.pumpkin/controller/IndexController.java
+++ src/main/java/com.pumpkin/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.pumpkin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,8 +12,17 @@ import javax.servlet.http.HttpServletRequest;
 public class IndexController {
 
         @RequestMapping("/toIndex")
-        public String goToIndexHtml(){
-                return "index";
+        public String goToIndexHtml(Integer in){
+                return "index.jsp";
         }
+
+//        @RequestMapping("/getString")
+//        public String getString(){
+//                //方法不进行跳转，只进行查询值并返回
+//        }
+
+
+        //写一个成功页面，登录成功。login.jsp  errorLogin.jsp
+        //写一个方法，成功的时候跳转哪个，失败的时候跳转哪一个
 
 }
---
+++ src/main/java/com.pumpkin/service/impl/CollectionImpl.java
@@ -0,0 +1,6 @@
+package com.pumpkin.service.impl;
+
+import com.pumpkin.service.CollectionInterface;
+
+public class CollectionImpl implements CollectionInterface {
+}
