a55176d0390a0fd1a75bfe4556a859325177c061
chenglihua1010 <1414592108@qq.com>
2020-06-22 20:10:11 +0800

    框架搭建

--- src/main/java/com/pumpkin/service/impl/VipImpl.java
+++ src/main/java/com/pumpkin/service/impl/VipImpl.java
@@ -1,6 +1,7 @@
 package com.pumpkin.service.impl;
 
 import com.pumpkin.entity.Vip;
+import com.pumpkin.service.VipInterface;
 
-public class VipImpl implements Vip {
+public class VipImpl implements VipInterface {
 }
