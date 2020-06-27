6c6c64765ebb92937ff5086cb66c2906473cf0b3
chenglihua1010 <1414592108@qq.com>
2020-06-27 17:27:17 +0800

    框架搭建

--- src/main/java/com/pumpkin/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/pumpkin/dao/impl/OrderDaoImpl.java
@@ -3,7 +3,9 @@ package com.pumpkin.dao.impl;
 import com.pumpkin.dao.BaseDao;
 import com.pumpkin.dao.OrderDao;
 import com.pumpkin.entity.Order;
+import org.springframework.stereotype.Repository;
 
+@Repository("orderDaoImpl")
 public class OrderDaoImpl extends BaseDaoImpl<Order> implements OrderDao{
         public void add(Order order){
                 save(order);
